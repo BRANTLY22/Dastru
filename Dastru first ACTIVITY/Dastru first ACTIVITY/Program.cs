ce739db6d0f68340cd547b15123e6b2a604b27f0
ACT-STUDENT <ACT-STUDENT@LAB3-PC41>
2025-11-26 08:52:07 +0800

    first act

---
+++ Dastru first ACTIVITY/Dastru first ACTIVITY/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dastru_first_ACTIVITY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name: Brantly Ordeniza
+            //Problem: Using Control Structures
+            //Section IT401A
+            Console.WriteLine("This program will determine what type of number and display its range.");
+
+            Console.Write("Please enter number: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+
+            if (num == 10)
+            {
+                Console.WriteLine("This is the range of numbers based from the number entered. \n 10 8 6 4 2");
+            }
+            else if (num == 8)
+            {
+                Console.WriteLine("This is the range of numbers based from the number entered. \n 10 8 6 4 2");
+            }
+            else if (num == 6)
+            {
+                Console.WriteLine("This is the range of numbers based from the number entered. \n 10 8 6 4 2");
+            }
+            else if (num == 4)
+            {
+                Console.WriteLine("This is the range of numbers based from the number entered. \n 10 8 6 4 2");
+            }
+            else if (num == 2)
+            {
+                Console.WriteLine("This is the range of numbers based from the number entered. \n 10 8 6 4 2");
+            }
+            else if (num >= 1)
+            {
+                Console.WriteLine("Sorry, I cannot provide the range of numbers based from the number you entered.");
+            }
+            else if (num >= 3)
+            {
+                Console.WriteLine("Sorry, I cannot provide the range of numbers based from the number you entered.");
+            }
+            else if (num >= 5)
+            {
+                Console.WriteLine("Sorry, I cannot provide the range of numbers based from the number you entered.");
+            }
+            else if (num >= 6)
+            {
+                Console.WriteLine("Sorry, I cannot provide the range of numbers based from the number you entered.");
+            }
+            else if (num >= 9)
+            {
+                Console.WriteLine("Sorry, I cannot provide the range of numbers based from the number you entered.");
+            }
+            else if(num >= 11)
+            {
+                Console.WriteLine("Sorry, I cannot provide the range of numbers based from the number you entered.");
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+    
+}
