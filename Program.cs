0f6e706bdcb0c79ac3bde6958a6dec0aa71c357e
BRATNTTT <ordenizabrantly@gmail.com>
2026-01-07 08:56:26 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordeniza_Act1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Array of Sport objects
+            Sports[] sports = new Sports[4];
+
+            // Initialize objects 
+            sports[0] = new Sports
+            {
+                Name = "Basketball",
+                ShapeType = "Round Ball",
+                Role = "Team Sport"
+            };
+
+            sports[1] = new Sports
+            {
+                Name = "Soccer",
+                ShapeType = "Round Ball",
+                Role = "Team Sport"
+            };
+
+            sports[2] = new Sports
+            {
+                Name = "Volleyball",
+                ShapeType = "Round Ball",
+                Role = "Team Sport"
+            };
+
+            sports[3] = new Sports
+            {
+                Name = "Tennis",
+                ShapeType = "Round Ball",
+                Role = "Individual or Doubles"
+            };
+            // Display section
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("HERE ARE THE SPORTS YOU LIKE\n");
+            Console.ResetColor();
+
+            foreach (Sports sport in sports)
+            {
+                sport.DisplayDetails();
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Sports.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordeniza_Act1
+{
+    internal class Sports
+    {
+        // Properties
+        public string Name { get; set; }
+        public string ShapeType { get; set; }
+        public string Role { get; set; }
+
+        // Method to display details
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Sport Name : " + Name);
+            Console.WriteLine("Shape Type : " + ShapeType);
+            Console.WriteLine("Role       : " + Role);
+            Console.WriteLine("***************************");
+        }
+
+    }
+}
