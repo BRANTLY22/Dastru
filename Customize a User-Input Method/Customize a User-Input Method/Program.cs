a1d9e0fa97c6ecd739ff139d44d10d98a50e106d
BRATNTTT <ordenizabrantly@gmail.com>
2026-01-23 08:36:11 +0800

    Add files via upload

---
+++ Customize a User-Input Method/Customize a User-Input Method/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customize_a_User_Input_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create 2x2 array for sports
+            PlayerSport[,] sports = new PlayerSport[2, 2];
+
+            Console.WriteLine("ENTER YOUR FAVORITE SPORTS\n");
+
+            //Input sports details
+            for (int i = 0; i < sports.GetLength(0); i++)
+            {
+                for (int j = 0; j < sports.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Sport [{i + 1},{j + 1}]");
+                    sports[i, j] = GetSportInput();
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine("HERE ARE THE SPORTS YOU LIKE:\n");
+
+            //Display sports details
+            for (int i = 0; i < sports.GetLength(0); i++)
+            {
+                for (int j = 0; j < sports.GetLength(1); j++)
+                {
+                    sports[i, j].DisplayPlayerDetails();
+                }
+            }
+
+            Console.ReadLine();//Pause program
+
+        }
+
+        //User input method
+        static PlayerSport GetSportInput()
+        {
+            PlayerSport sport = new PlayerSport();
+
+            Console.Write("Enter sport name: ");
+            sport.Name = Console.ReadLine();
+
+            Console.Write("Enter shape type: ");
+            sport.ShapeType = Console.ReadLine();
+
+            Console.Write("Enter sport role (Team/Individual): ");
+            sport.Role = Console.ReadLine();
+
+            Console.Write("Enter player name: ");
+            sport.PlayerName = Console.ReadLine();
+
+            return sport;
+
+        }
+    }
+}
---
+++ Customize a User-Input Method/Customize a User-Input Method/Sports.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customize_a_User_Input_Method
+{
+    internal class Sports
+    {
+        public string Name { get; set; }
+        public string ShapeType { get; set; }
+        public string Role { get; set; }
+
+
+        //Show sport details
+        public void DisplayDetails()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Sport Name : " + Name);
+            Console.WriteLine("Shape Type : " + ShapeType);
+            Console.WriteLine("Sport Role : " + Role);
+        }
+    }
+    internal class PlayerSport : Sports
+    {
+        public string PlayerName { get; set; }
+
+        //Show sport and player details
+        public void DisplayPlayerDetails()
+        {
+            DisplayDetails();
+            Console.WriteLine("Player Name : " + PlayerName);
+            Console.WriteLine("-----------------------------");
+        }
+    }
+}
+    
